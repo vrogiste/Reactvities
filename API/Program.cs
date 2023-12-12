1091bb2eba8998de17ad4d44c4a014ad72770430
Vincent Rogister <vincent.rogister@itgration.be>
2023-12-12 18:54:45 +0100

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -15,6 +15,14 @@ builder.Services.AddDbContext<DataContext>(opt =>
 }
 );
 
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy("CorsPolicy", policy =>
+    {
+        policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -23,6 +31,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
