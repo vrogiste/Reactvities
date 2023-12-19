8770f05910e3e240728d4697ee39de95f3915fe8
Vincent Rogister <vincent.rogister@itgration.be>
2023-12-19 10:20:43 +0100

    Section 4 partia

---
+++ Application/Activities/List.cs
@@ -0,0 +1,12 @@
+using Domain;
+//using MediatR;
+using System.Diagnostics;
+using System.Net;
+
+namespace Application.Activities
+{
+    public class List
+    {
+        //public class Query : IRequest<List<Activity>>
+    }
+}
